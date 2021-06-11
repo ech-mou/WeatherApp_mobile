a42b0182a2e51bfa0ecd91f12d336e6891b6274c
ech-mou <echmouhsine1994@gmail.com>
2021-06-11 11:26:18 +0100

    application mobile

---
+++ WeatherApp/app/src/main/java/com/example/tutorialweatherapp/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.tutorialweatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Icon;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "c1269f0c761ea87f38c2c89457aae995";
+
+    Button btnSearch;
+    EditText etCityName;
+    ImageView iconWeather;
+    TextView tvTemp,tvCity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        btnSearch = findViewById(R.id.btnSearch);
+        etCityName = findViewById(R.id.etCityName);
+        iconWeather = findViewById(R.id.iconWeather);
+        tvTemp = findViewById(R.id.tvTemp);
+        tvCity = findViewById(R.id.tvCity);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city= etCityName.getText().toString();
+                if(city.isEmpty())
+                    Toast.makeText(MainActivity.this,"Please entrer a city name",Toast.LENGTH_SHORT).show();
+                else {
+                    //TODO:load weather by city name !
+                    loadWeatherByCityName(city);
+                }
+            }
+        });
+
+    }
+
+    private void loadWeatherByCityName(String city) {
+        Ion.with(this)
+                    .load("http://api.openweathermap.org/data/2.5/weather?q="+city+"&&units=metric&appid="+API_KEY)
+                .asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonObject result) {
+                        // do stuff with the result or error
+                        if(e!=null){
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this,"server error",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            //convert json response to java
+                            JsonObject main = result.get("main").getAsJsonObject();
+                            double temp = main.get("temp").getAsDouble();
+                            tvTemp.setText(temp+"°C");
+
+                            JsonObject sys=result.get("sys").getAsJsonObject();
+                            String country=sys.get("country").getAsString();
+                            tvCity.setText(city+", "+country);
+
+                            JsonArray weather = result.get("weather").getAsJsonArray();
+                            String icon=weather.get(0).getAsJsonObject().get("icon").getAsString();
+                            loadIcon(icon);
+
+                        }
+                    }
+                });
+    }
+
+    private void loadIcon(String icon) {
+        Ion.with(this)
+                .load("https://openweathermap.org/img/w/"+icon+".png")
+                .intoImageView(iconWeather);
+    }
+}
